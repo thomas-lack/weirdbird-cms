b72f2869d07bea5f573882cda93cb92263203fd4
post.thomas.lack <Post.Thomas.Lack@gmail.com>
2012-12-08 18:02:44 +0000

    * started working on the treeview representation

--- application/classes/controller/cms/articles.php
+++ application/classes/controller/cms/articles.php
@@ -8,28 +8,6 @@ class Controller_CMS_Articles extends Controller_CMS_Main
 	public function action_index() 
 	{
 		$this->template->category = 'Articles';
-
-		
-		// get needed information about modules and layouts
-		$structures = ORM::factory('structure')->where('active','=','true')->order_by('position','asc')->find_all();
-		$template = ORM::factory('template')->where('active','=','1')->find();
-		$layouts = ORM::factory('layout')->where('template_id','=',$template->id)->find_all();
-		$modules = ORM::factory('module')->where('template_id','=',$template->id)->find_all();
-
-		// propose data to the view
-		$this->template->structures = $structures;
-		$this->template->layouts = $layouts;
-		$this->template->modules = $modules;
-		$this->template->mappings = array();
-
-		// get all needed module <-> column mappings
-		foreach ($structures as $s) {
-			$mappings = ORM::factory('structurecolumnmapping')->where('structures_id','=',$s->id)->find_all();
-			foreach($mappings as $m) {
-				array_push($this->template->mappings, $m);
-			}
-		}
-		
 	}
 
 	public function action_activestructures()
--- application/views/cms/structures/template.php
+++ application/views/cms/structures/template.php
@@ -1,24 +1,6 @@
 <p class="bottom-padding-10">
 	<span class="sans-serif very-small uppercase dark-gray"><?= $category; ?></span>
 </p>
-<!--<h1>Site structure and category management</h1>-->
-
-<!--<div id="structures-grid" style="margin-top:20px;"></div>-->
 
 <h1>Structures Management</h1>
 <div id="structures-form" style="margin-top:20px;"></div>
-
-<!--<table class="tbl-fullwidth">
-	<tr>
-		<td style="width:60%;">
-			<h1>Structures Management</h1>
-			<div id="structures-form" style="margin-top:20px;"></div>
-		</td>
-		<td>
-			<h1>Layout and module selection</h1>
-			<div id="layout-module-edit-grid" style="margin-top:20px;">
-				<span class="icon big">"</span> Please select a category first.
-			</div>
-		</td>
-	</tr>
-</table>-->
