b402af8a2cb0818f400932a53cadfa6a7887cdb9
Thomas Lack <thomas.lack@hypoport.de>
2019-07-21 15:37:28 +0200

    Fix variable not found error
    * Autoformatter settings applied

--- application/classes/Controller/Cms/Structurecolumnmappings.php
+++ application/classes/Controller/Cms/Structurecolumnmappings.php
@@ -2,54 +2,55 @@
 
 class Controller_Cms_StructureColumnMappings extends Controller_Cms_Data
 {
-	public function action_index() {}
+    public function action_index()
+    {
+    }
 
-	public function action_read()
-	{
-    $objToArray = function($obj) {
-      return $obj->as_array();
-    };
-		$result = array_map(
-      objToArray,
-			ORM::factory('StructureColumnMapping')->find_all()->as_array()		
-		);
-		
-		$this->template->result = $result;
-	}
+    public function action_read()
+    {
+        $objToArray = function ($obj) {
+            return $obj->as_array();
+        };
 
-	public function action_update()		// works as UPDATE and CREATE method
-	{
-		$data = $this->request->post();
-		
-		$mapping = ORM::factory('StructureColumnMapping')
-			->where('structure_id','=',$data['structure_id'])
-			->where('column','=',$data['column'])
-			->find();
-		// in case we cannot UPDATE we insert all the data again
-		// to CREATE a new relation
-		$mapping->structure_id = $data['structure_id'];
-		$mapping->column = $data['column'];
-		$mapping->module_id = $data['module_id'];
-		$mapping->save();
+        $result = array_map(
+            $objToArray,
+            ORM::factory('StructureColumnMapping')->find_all()->as_array()
+        );
 
-		// since we did a possible update of a module, it is possible, that
-		// previously connected articles are not allowed anymore
-		// -> mark those articles as 'orphaned' by deleting the mapping id
-		$module = ORM::factory('Module', $mapping->module_id);
-		if ($module->allowarticles == 0) 
-		{
-			$articles = ORM::factory('Article')
-				->where('structure_column_mapping_id','=',$mapping->id)
-				->find_all();
+        $this->template->result = $result;
+    }
 
-			foreach($articles as $article)
-			{
-				$article->structure_column_mapping_id = null;
-				$article->active = 0;
-				$article->save();
-			}
-		}
+    public function action_update()        // works as UPDATE and CREATE method
+    {
+        $data = $this->request->post();
 
-		$this->template->result = array( 'success' => true );
-	}
+        $mapping = ORM::factory('StructureColumnMapping')
+            ->where('structure_id', '=', $data['structure_id'])
+            ->where('column', '=', $data['column'])
+            ->find();
+        // in case we cannot UPDATE we insert all the data again
+        // to CREATE a new relation
+        $mapping->structure_id = $data['structure_id'];
+        $mapping->column = $data['column'];
+        $mapping->module_id = $data['module_id'];
+        $mapping->save();
+
+        // since we did a possible update of a module, it is possible, that
+        // previously connected articles are not allowed anymore
+        // -> mark those articles as 'orphaned' by deleting the mapping id
+        $module = ORM::factory('Module', $mapping->module_id);
+        if ($module->allowarticles == 0) {
+            $articles = ORM::factory('Article')
+                ->where('structure_column_mapping_id', '=', $mapping->id)
+                ->find_all();
+
+            foreach ($articles as $article) {
+                $article->structure_column_mapping_id = null;
+                $article->active = 0;
+                $article->save();
+            }
+        }
+
+        $this->template->result = array('success' => true);
+    }
 }
